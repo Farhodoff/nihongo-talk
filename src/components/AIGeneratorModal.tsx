9745a077f81075c2da55946e0dcf217e7624c3b0
Farhod <fsoyilov@gmail.com>
2026-02-11 15:03:56 +0500

    feat: Add 100 flashcard generation option
    - Added 100 cards option to AI Generator dropdown
    - Users can now generate up to 100 flashcards at once
    - Note: Large batches may take longer to process

--- src/components/AIGeneratorModal.tsx
+++ src/components/AIGeneratorModal.tsx
@@ -132,6 +132,7 @@ const AIGeneratorModal: React.FC<AIGeneratorModalProps> = ({ isOpen, onClose, su
                                     <option value={30}>30</option>
                                     <option value={40}>40</option>
                                     <option value={50}>50</option>
+                                    <option value={100}>100</option>
                                 </select>
                             </div>
 
