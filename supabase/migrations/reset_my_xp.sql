fb2276de54fb5477398ab5ede02ad10e98259f5d
Farhod Soyilov <fsoyilov@gmail.com>
2026-03-01 17:12:18 +0500

    chore: final root cleanup, move scripts/docs, sequence sql migrations

--- supabase/migrations/reset_my_xp.sql
+++
@@ -1,9 +0,0 @@
--- Reset current user's XP and Level to 0
--- This will reset your gamification progress
-
-UPDATE profiles
-SET 
-    total_xp = 0,
-    level = 1,
-    current_streak = 0
-WHERE id = auth.uid();
