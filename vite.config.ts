c94a1ffa0982109f947770851fde244a5305792d
Farhodoff <fsoyilovv@gmail.com>
2026-05-02 20:47:36 +0500

    PWA: Mobil tajriba uchun splash screen va Workbox keshlash strategiyasi sozlandi

--- vite.config.ts
+++ vite.config.ts
@@ -15,22 +15,60 @@ export default defineConfig({
                 short_name: 'StudyPlanner',
                 description: 'Intelligent companion for academic success',
                 theme_color: '#111827',
+                background_color: '#111827',
+                display: 'standalone',
+                orientation: 'portrait',
                 icons: [
                     {
                         src: 'favicon.png',
                         sizes: '192x192',
-                        type: 'image/png'
-                    },
-                    {
-                        src: 'favicon.png',
-                        sizes: '512x512',
-                        type: 'image/png'
+                        type: 'image/png',
+                        purpose: 'any'
                     },
                     {
                         src: 'favicon.png',
                         sizes: '512x512',
                         type: 'image/png',
-                        purpose: 'any maskable'
+                        purpose: 'any'
+                    },
+                    {
+                        src: 'favicon.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable'
+                    }
+                ]
+            },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+                runtimeCaching: [
+                    {
+                        urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'google-fonts-cache',
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365 // 1 yil
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200]
+                            }
+                        }
+                    },
+                    {
+                        urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+                        handler: 'CacheFirst',
+                        options: {
+                            cacheName: 'gstatic-fonts-cache',
+                            expiration: {
+                                maxEntries: 10,
+                                maxAgeSeconds: 60 * 60 * 24 * 365
+                            },
+                            cacheableResponse: {
+                                statuses: [0, 200]
+                            }
+                        }
                     }
                 ]
             }
