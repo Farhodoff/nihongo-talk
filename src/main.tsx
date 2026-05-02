5dd063de8d93de16c6b9cf86916c44784167ea41
Farhodoff <fsoyilovv@gmail.com>
2026-05-02 19:44:20 +0500

    PWA: Service worker main.tsx faylida ro'yxatdan o'tkazildi

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { registerSW } from 'virtual:pwa-register';
 
 import './index.css';
+
+// PWA service worker-ni ro'yxatdan o'tkazish
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
