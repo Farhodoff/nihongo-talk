c144be1d8434c989645c95750f2617c35348ac37
Farhodoff <fsoyilovv@gmail.com>
2026-05-17 01:44:14 +0500

    theme.ts dagi react-native bog'liqligi olib tashlandi (vebga moslashtirildi)

--- constants/theme.ts
+++ constants/theme.ts
@@ -3,7 +3,12 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-import { Platform } from 'react-native';
+// Web compatible Platform mock
+const Platform = {
+  OS: 'web',
+  select: (objs: any) => objs.web || objs.default,
+};
+
 
 const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
